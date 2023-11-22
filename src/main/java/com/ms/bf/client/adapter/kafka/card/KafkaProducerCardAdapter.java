e019a7af2f8494884bd16b065ed0d467f9bc28e7
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-22 20:25:50 -0300

    Update KafkaProducerCardAdapter.java

--- src/main/java/com/ms/bf/client/adapter/kafka/card/KafkaProducerCardAdapter.java
+++ src/main/java/com/ms/bf/client/adapter/kafka/card/KafkaProducerCardAdapter.java
@@ -1,4 +1,16 @@
 package com.ms.bf.client.adapter.kafka.card;
 
-public class KafkaProducerCardAdapter {
+import com.ms.bf.client.domain.Card;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class KafkaProducerCardAdapter implements KafkaProducerCardPort{
+    @Override
+    public UUID sendCardRequest(Card card) {
+        return null;
+    }
 }
